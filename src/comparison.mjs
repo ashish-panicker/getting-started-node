7706c383309d724c97a2620321dde0e55c28e011
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-06 15:29:15 +0530

    Arrow functions

--- src/comparison.mjs
+++ src/comparison.mjs
@@ -1,10 +1,8 @@
-export const findLargest = function (a, b) {
-  return a > b ? a : b;
-};
+export const findLargest =  (a, b) => a > b ? a : b
 
-export const findSmallest = function (a, b) {
-  return a < b ? a : b;
-};
+export const findSmallest =  (a, b) => {
+  return a < b ? a : b
+}
 
 // CJS syntax
 // module.exports = { findLargest, findSmallest };
